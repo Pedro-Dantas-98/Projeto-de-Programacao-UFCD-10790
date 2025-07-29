eed8bb3ec05caddde5dc2babca6aa6a2ae09da02
Pedro Dantas <pedrodantas@live.com.pt>
2025-07-29 22:27:48 +0100

    Botões

--- ui.py
+++ ui.py
@@ -1,32 +1,47 @@
 import tkinter as tk
+from tkinter import ttk
 
 class MenuPrincipal(tk.Tk):
     def __init__(self):
         super().__init__()
         
+        #Janela
         self.title("Projeto UFCD 10790")
         self.resizable(False, False)
-
         larguraEcra = self.winfo_screenwidth()
         alturaEcra = self.winfo_screenheight()
-        larguraJanela = 130
-        alturaJanela = 94
+        larguraJanela = 800
+        alturaJanela = 400
         centerX = (larguraEcra - larguraJanela) // 2
         centerY = (alturaEcra - alturaJanela) // 2
         self.geometry(f'{larguraJanela}x{alturaJanela}+{centerX}+{centerY}')
 
+        #Bases de Dados
         frameBD = tk.Frame(self, padx = 2, pady = 2)
-        frameBD.pack()
-
-        textoBD = tk.Label(self, text = "Selecione uma lista:")
+        frameBD.pack(anchor=tk.NW)
+        textoBD = tk.Label(frameBD, text = "Selecione uma lista:")
         textoBD.pack(anchor = tk.NW)
-
-        listaBD = tk.Listbox(self, width = 20, height = 4)
-        listaBD.insert(0, "Jogos")
-        listaBD.insert(1, "Filmes")
-        listaBD.insert(2, "Series")
+        listaBD = tk.Listbox(frameBD, width = 17, height = 3)
+        listaBD.insert(0, " Jogos")
+        listaBD.insert(1, " Filmes")
+        listaBD.insert(2, " Series")
         listaBD.bind('<Double-1>', self.selecionarBD)
-        listaBD.pack()
+        listaBD.pack(anchor=tk.NW, padx = 2, pady = 2)
+        
+        #Botões
+        frameBotoes = tk.Frame(self)
+        frameBotoes.pack(side=tk.BOTTOM, anchor=tk.W, pady = 10)
+
+        botaoRegistar = tk.Button(frameBotoes, text = "Registar", width = 14, height = 3)
+        botaoRegistar.pack(anchor=tk.SW, padx = 4, pady = 2)
+        botaoEditar = tk.Button(frameBotoes, text = "Editar", width = 14, height = 3)
+        botaoEditar.pack(anchor=tk.SW, padx = 4, pady = 2)
+        botaoVisualizar = tk.Button(frameBotoes, text = "Visualizar", width = 14, height = 3)
+        botaoVisualizar.pack(anchor=tk.SW, padx = 4, pady = 2)
+        botaoEstatistica = tk.Button(frameBotoes, text = "Estatísticas", width = 14, height = 3)
+        botaoEstatistica.pack(anchor=tk.SW, padx = 4, pady = 2)
+        botaoBackup = tk.Button(frameBotoes, text = "Backup", width = 14, height = 3)
+        botaoBackup.pack(anchor=tk.SW, padx = 4, pady = 2)
 
     def selecionarBD(self, event):
         print("Base de dados selecionada")
