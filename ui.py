c5c1027bf45c8785428a28920b1ea37d534a4ae9
Pedro Dantas <pedrodantas@live.com.pt>
2025-07-29 22:52:16 +0100

    Placeholder Barra de Pesquisa
    Não está a aparecer corretamente, este commit é para poder fazer rollback caso necessário

--- ui.py
+++ ui.py
@@ -18,7 +18,7 @@ class MenuPrincipal(tk.Tk):
 
         #Bases de Dados
         frameBD = tk.Frame(self, padx = 2, pady = 2)
-        frameBD.pack(anchor=tk.NW)
+        frameBD.pack(anchor = tk.NW)
         textoBD = tk.Label(frameBD, text = "Selecione uma lista:")
         textoBD.pack(anchor = tk.NW)
         listaBD = tk.Listbox(frameBD, width = 17, height = 3)
@@ -30,18 +30,28 @@ class MenuPrincipal(tk.Tk):
         
         #Botões
         frameBotoes = tk.Frame(self)
-        frameBotoes.pack(side=tk.BOTTOM, anchor=tk.W, pady = 10)
+        frameBotoes.pack(side = tk.BOTTOM, anchor = tk.W, pady = 10)
 
         botaoRegistar = tk.Button(frameBotoes, text = "Registar", width = 14, height = 3)
-        botaoRegistar.pack(anchor=tk.SW, padx = 4, pady = 2)
+        botaoRegistar.pack(anchor = tk.SW, padx = 4, pady = 2)
         botaoEditar = tk.Button(frameBotoes, text = "Editar", width = 14, height = 3)
-        botaoEditar.pack(anchor=tk.SW, padx = 4, pady = 2)
+        botaoEditar.pack(anchor = tk.SW, padx = 4, pady = 2)
         botaoVisualizar = tk.Button(frameBotoes, text = "Visualizar", width = 14, height = 3)
-        botaoVisualizar.pack(anchor=tk.SW, padx = 4, pady = 2)
+        botaoVisualizar.pack(anchor = tk.SW, padx = 4, pady = 2)
         botaoEstatistica = tk.Button(frameBotoes, text = "Estatísticas", width = 14, height = 3)
-        botaoEstatistica.pack(anchor=tk.SW, padx = 4, pady = 2)
+        botaoEstatistica.pack(anchor = tk.SW, padx = 4, pady = 2)
         botaoBackup = tk.Button(frameBotoes, text = "Backup", width = 14, height = 3)
-        botaoBackup.pack(anchor=tk.SW, padx = 4, pady = 2)
+        botaoBackup.pack(anchor = tk.SW, padx = 4, pady = 2)
+        
+        #Barra de pesquisa
+        framePesquisa = tk.Frame(self, bg="yellow")
+        framePesquisa.pack(side=tk.TOP, anchor=tk.NE, padx=10, pady=5)
+
+        barraPesquisa = tk.Entry(framePesquisa, width=30)
+        barraPesquisa.pack(side=tk.RIGHT)
+
+        textoPesquisa = tk.Label(framePesquisa, text="Pesquisar:")
+        textoPesquisa.pack(side=tk.RIGHT, padx=5)
 
     def selecionarBD(self, event):
         print("Base de dados selecionada")
