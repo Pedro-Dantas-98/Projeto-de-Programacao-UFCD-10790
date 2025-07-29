b398f52437a652b7405680b8d120e509150ea618
Pedro Dantas <pedrodantas@live.com.pt>
2025-07-29 23:16:38 +0100

    Mudança de pack para grid
    Certos elementos não estavam a ser posicionados dentro da janela por isso mudei para uma abordagem com grid

--- ui.py
+++ ui.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import ttk
 
 class MenuPrincipal(tk.Tk):
     def __init__(self):
@@ -15,43 +14,47 @@ class MenuPrincipal(tk.Tk):
         centerX = (larguraEcra - larguraJanela) // 2
         centerY = (alturaEcra - alturaJanela) // 2
         self.geometry(f'{larguraJanela}x{alturaJanela}+{centerX}+{centerY}')
+        self.grid_columnconfigure(1, weight = 1)
+        self.grid_rowconfigure(1, weight = 1)  
 
         #Bases de Dados
-        frameBD = tk.Frame(self, padx = 2, pady = 2)
-        frameBD.pack(anchor = tk.NW)
+        frameBD = tk.Frame(self)
+        frameBD.grid(row = 0, column = 0, sticky = "nw", padx = 2, pady = (4, 0))
+        
         textoBD = tk.Label(frameBD, text = "Selecione uma lista:")
         textoBD.pack(anchor = tk.NW)
+        
         listaBD = tk.Listbox(frameBD, width = 17, height = 3)
         listaBD.insert(0, " Jogos")
         listaBD.insert(1, " Filmes")
         listaBD.insert(2, " Series")
         listaBD.bind('<Double-1>', self.selecionarBD)
-        listaBD.pack(anchor=tk.NW, padx = 2, pady = 2)
+        listaBD.pack(anchor = tk.NW, padx = 2, pady = 2)
         
         #Botões
         frameBotoes = tk.Frame(self)
-        frameBotoes.pack(side = tk.BOTTOM, anchor = tk.W, pady = 10)
+        frameBotoes.grid(row = 2, column = 0, sticky = "sw", pady = (0, 5))
 
         botaoRegistar = tk.Button(frameBotoes, text = "Registar", width = 14, height = 3)
-        botaoRegistar.pack(anchor = tk.SW, padx = 4, pady = 2)
+        botaoRegistar.pack(anchor = tk.W, padx = 4, pady = 2)
         botaoEditar = tk.Button(frameBotoes, text = "Editar", width = 14, height = 3)
-        botaoEditar.pack(anchor = tk.SW, padx = 4, pady = 2)
+        botaoEditar.pack(anchor = tk.W, padx = 4, pady = 2)
         botaoVisualizar = tk.Button(frameBotoes, text = "Visualizar", width = 14, height = 3)
-        botaoVisualizar.pack(anchor = tk.SW, padx = 4, pady = 2)
+        botaoVisualizar.pack(anchor = tk.W, padx = 4, pady = 2)
         botaoEstatistica = tk.Button(frameBotoes, text = "Estatísticas", width = 14, height = 3)
-        botaoEstatistica.pack(anchor = tk.SW, padx = 4, pady = 2)
+        botaoEstatistica.pack(anchor = tk.W, padx = 4, pady = 2)
         botaoBackup = tk.Button(frameBotoes, text = "Backup", width = 14, height = 3)
-        botaoBackup.pack(anchor = tk.SW, padx = 4, pady = 2)
+        botaoBackup.pack(anchor = tk.W, padx = 4, pady = 2)
         
         #Barra de pesquisa
-        framePesquisa = tk.Frame(self, bg="yellow")
-        framePesquisa.pack(side=tk.TOP, anchor=tk.NE, padx=10, pady=5)
+        framePesquisa = tk.Frame(self)
+        framePesquisa.grid(row = 0, column = 2, sticky = "ne", padx = 10, pady = (5, 0))
+        
+        textoPesquisa = tk.Label(framePesquisa, text = "Pesquisar:")
+        textoPesquisa.pack(side = tk.LEFT)
 
-        barraPesquisa = tk.Entry(framePesquisa, width=30)
-        barraPesquisa.pack(side=tk.RIGHT)
-
-        textoPesquisa = tk.Label(framePesquisa, text="Pesquisar:")
-        textoPesquisa.pack(side=tk.RIGHT, padx=5)
+        barraPesquisa = tk.Entry(framePesquisa, width = 30)
+        barraPesquisa.pack(side = tk.LEFT)
 
     def selecionarBD(self, event):
         print("Base de dados selecionada")
